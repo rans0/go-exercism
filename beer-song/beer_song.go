9a837785c8ece52176f05ca86c515f567ffaafe8
rans0 <kharansyah1@gmail.com>
2021-01-28 22:38:19 +0700

    beer song

---
+++ beer-song/beer_song.go
@@ -0,0 +1,50 @@
+package beer
+
+import (
+	"errors"
+	"fmt"
+)
+
+var verses = [3]string{
+	"No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n",
+	"1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n",
+	"%d bottles of beer on the wall, %d bottles of beer.\nTake one down and pass it around, %d bottle%s of beer on the wall.\n",
+}
+
+// Verse recites the selected verse of the song
+func Verse(i int) (verse string, err error) {
+	switch {
+	case i == 0, i == 1:
+		verse = verses[i]
+	case i == 2:
+		verse = fmt.Sprintf(verses[2], i, i, i-1, "")
+	case i > 2 && i < 100:
+		verse = fmt.Sprintf(verses[2], i, i, i-1, "s")
+	default:
+		verse, err = "", errors.New("invalid verse")
+	}
+	return verse, err
+}
+
+// Verses returns several verses
+func Verses(upper, lower int) (string, error) {
+	var verses string
+	if lower >= upper {
+		return "", errors.New("invalid verse")
+	}
+
+	for i := upper; i >= lower; i-- {
+		v, err := Verse(i)
+		if err != nil {
+			return "", errors.New("invalid verse")
+		}
+		verses += v + "\n"
+	}
+	return verses, nil
+}
+
+// Song returns all the verses
+func Song() string {
+	verses, _ := Verses(99, 0)
+	return verses
+}
