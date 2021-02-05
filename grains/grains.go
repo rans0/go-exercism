456926bfadea39ac68113612350cebf6a8a4acb9
rans0 <kharansyah1@gmail.com>
2021-02-05 23:45:59 +0700

    grains

---
+++ grains/grains.go
@@ -0,0 +1,19 @@
+package grains
+
+import (
+	"errors"
+)
+
+const MaxUint64 = (1 << 64) - 1 // << = x × 2y
+
+func Total () uint64 {
+	return uint64(MaxUint64)
+}
+
+func Square (input int) (expected uint64, err error) {
+	if 1 > input || input > 64 {
+		return uint64(0), errors.New("invalid")
+	}
+
+	return uint64(1 << uint(input-1)), nil
+}
